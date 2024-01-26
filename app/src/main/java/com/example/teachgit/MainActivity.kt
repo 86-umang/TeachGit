79e40d0ac4790002fae423401d96ab43ea87d602
Umangkumar Patel <umangkumar.18beceg031@gmail.com>
2024-01-26 14:26:24 -0500

    first

---
+++ app/src/main/java/com/example/teachgit/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.teachgit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
